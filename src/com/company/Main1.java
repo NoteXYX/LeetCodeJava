e9737ffe19007fdfdd7b17ba7226fc5220e5c06c
NoteXYX <707997228@qq.com>
2020-04-06 11:28:36 +0800

    Initial commit

---
+++ src/com/company/Main1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>(2);
+        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 3);
+        System.out.println(map);
+    }
+}
