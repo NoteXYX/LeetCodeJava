d2e5686f3e0f44a7fcd1ebd517307526351cccae
NoteXYX <707997228@qq.com>
2020-04-09 14:45:08 +0800

    ok

---
+++ src/com/company/Main3.java
@@ -0,0 +1,22 @@
+//无重复字符的最长子串
+package com.company;
+import java.util.HashSet;
+
+public class Main3 {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        HashSet<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+}
