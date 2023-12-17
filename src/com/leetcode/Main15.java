c66fb1be5e1f67ab60e8992fe1fa7b9e8cd9349e
谢育欣 <xyxhfut@qq.com>
2023-12-17 16:00:27 +0800

    done

--- src/com/leetcode/Main15.java
+++ src/com/leetcode/Main15.java
@@ -15,31 +15,32 @@ import java.util.List;
 public class Main15 {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums==null || nums.length<3)
+        if (nums == null || nums.length < 3) {
             return res;
+        }
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length-2; i++) {
-            if (nums[i] > 0)
-                break;
-            if (i > 0 && nums[i]==nums[i-1])
-                continue;
+        for (int i = 0; i < nums.length - 2; i++) {
             int left = i + 1;
-            int right = nums.length-1;
+            int right = nums.length - 1;
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left<right && nums[right] == nums[right-1])
-                        right--;
-                    while (left<right && nums[left] == nums[left+1])
-                        left++;
-                    left++;
-                    right--;
-                }
-                else if (sum < 0)
-                    left++;
-                else if (sum>0)
-                    right--;
+               if (left != i + 1 && nums[left] == nums[left - 1]) {
+                   left++;
+               } else if (right != nums.length - 1 && nums[right] == nums[right + 1]) {
+                   right--;
+               } else {
+                   if (nums[i] + nums[left] + nums[right] > 0) {
+                       right--;
+                   } else if (nums[i] + nums[left] + nums[right] < 0) {
+                       left++;
+                   } else {
+                       res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                       left++;
+                       right--;
+                   }
+               }
             }
         }
         return res;
